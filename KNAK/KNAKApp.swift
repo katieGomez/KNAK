41baea3f8aaa0581668064bcc30efe8ab91a6670
Katie Gomez <kgomez0177@gmail.com>
2023-07-18 13:17:34 -0700

    Initial Commit

---
+++ KNAK/KNAKApp.swift
@@ -0,0 +1,17 @@
+//
+//  KNAKApp.swift
+//  KNAK
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct KNAKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
